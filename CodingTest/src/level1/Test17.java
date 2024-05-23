7a8ff45cfb3cadfa693790982eab44eb3d27ba8d
Siu Ahn <jumia011@gmail.com>
2024-05-23 23:55:42 +0900

    [create] Test17.java

---
+++ CodingTest/src/level1/Test17.java
@@ -0,0 +1,22 @@
+package level1;
+
+public class Test17 {
+	class Solution {
+		//JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은
+		//소문자인 문자열입니다. 단, 첫 문자가 알파벳이 아닐 때에는 이어지는
+		//알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+		//문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 
+		//리턴하는 함수, solution을 완성해주세요.
+	    public String solution(String s) {
+			String[] arr = s.toLowerCase().split("");
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < arr.length; i++) {
+				if(arr[i].equals(" ")) {
+					arr[i+1] = arr[i+1].toUpperCase();
+				}
+				sb.append(arr[i]);
+			}
+	        return sb.toString();
+	    }
+	}
+}

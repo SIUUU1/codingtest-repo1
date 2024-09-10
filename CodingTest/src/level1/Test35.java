30eab6df27c123da50e9475421a3344c4791202d
Siu Ahn <jumia011@gmail.com>
2024-09-10 23:16:28 +0900

    [create] create /level1/Test35 푸드파이터

---
+++ CodingTest/src/level1/Test35.java
@@ -0,0 +1,21 @@
+package level1;
+
+//푸드 파이터
+public class Test35 {
+	class Solution {
+		public String solution(int[] food) {
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 1; i < food.length; i++) {
+				int count = food[i] / 2;
+				sb.append(String.valueOf(i).repeat(count));
+			}
+
+			String answer = sb + "0";
+			answer += sb.reverse();
+
+			return answer;
+		}
+	}
+}

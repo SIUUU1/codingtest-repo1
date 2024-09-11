e33c9a41532b2ffdce7372b5bbe6518ac4646b94
Siu Ahn <jumia011@gmail.com>
2024-09-11 23:37:38 +0900

    [create] create /level1/Test36 2016년

---
+++ CodingTest/src/level1/Test36.java
@@ -0,0 +1,20 @@
+package level1;
+
+public class Test36 {
+//2016년 요일 구하기
+	class Solution {
+		public String solution(int a, int b) {
+			String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+			int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			int total = 0;
+
+			for (int i = 0; i < a - 1; i++) {
+				total += date[i];
+			}
+			total += b - 1;
+
+			return day[total % 7];
+
+		}
+	}
+}

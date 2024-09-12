7cd171d200095cffecee92f03d3170b20e635f3f
Siu Ahn <jumia011@gmail.com>
2024-09-12 23:46:09 +0900

    [create] create /level2/Test16 모음사전

---
+++ CodingTest/src/level2/Test16.java
@@ -0,0 +1,44 @@
+package level2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//모음사전
+public class Test16 {
+	class Solution {
+		final char[] WORDS = { 'A', 'E', 'I', 'O', 'U' };
+		final int MAX_LENGTH = 5;
+
+		public int solution(String word) {
+			int answer = 0;
+
+			List<String> elements = new ArrayList<>();
+
+			for (int i = 0; i < WORDS.length; i++) {
+				dfs(elements, String.valueOf(WORDS[i]));
+			}
+
+			for (int i = 0; i < elements.size(); i++) {
+				if (elements.get(i).equals(word)) {
+					answer = i + 1;
+					break;
+				}
+			}
+
+			return answer;
+		}
+		
+		//깊이우선탐색
+		void dfs(List<String> elements, String str) {
+			if (str.length() > MAX_LENGTH)
+				return;
+
+			if (!elements.contains(str))
+				elements.add(str);
+
+			for (int i = 0; i < WORDS.length; i++) {
+				dfs(elements, str + WORDS[i]);
+			}
+		}
+	}
+}

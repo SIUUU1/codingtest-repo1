d4698fb5bf4172a1dde0e6a1d6d5f21ce76cfebc
Siu Ahn <jumia011@gmail.com>
2024-10-01 23:43:32 +0900

    [create] create /level1/Test43 문자열 나누기

---
+++ CodingTest/src/level1/Test43.java
@@ -0,0 +1,27 @@
+package level1;
+
+//문자열 나누기
+public class Test43 {
+	class Solution {
+		public int solution(String s) {
+			int answer = 1;
+
+			char x = s.charAt(0);
+			int count = 1;
+			for (int i = 1; i < s.length(); i++) {
+				if (count == 0) {
+					answer++;
+					x = s.charAt(i);
+				}
+
+				if (x == s.charAt(i)) {
+					count++;
+				} else {
+					count--;
+				}
+			}
+
+			return answer;
+		}
+	}
+}
